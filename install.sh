20b469ed4f1cf928db6ba53b546040e1329cb9f0
Gonçal Sallan i Baso <zonadart.quicksilver@gmail.com>
2024-02-06 00:01:48 +0100

    MAN spanish i altres

--- install.sh
+++ install.sh
@@ -18,36 +18,23 @@
 #===============================================================================
 
 set -o nounset                              # Treat unset variables as an error
-set -eu
+
 # IF DEBIAN
-# if [ -e "/etc/debian_version" ]; then
-    echo "Copiant zonadart a /opt/zonadart ..."
+if [ -e "/etc/debian_version" ]; then
+    echo "Copying zonadart to /usr/local/zonadart ..."
     # check for older version
-    id=zonadart
-#    if [ -d "/opt/zonadart/" ]; then
-				sudo rm -f /usr/local/bin/zonadart
-        sudo rm -rf /opt/zonadart
-#    fi
+    id=$(id -u)
+    if [ -d "/usr/local/zonadart/" ]; then
+        sudo rm -f /usr/bin/zonadart
+        sudo rm -rf /usr/local/zonadart
+    fi
     # install essential files
-    sudo mkdir /opt/zonadart/
-    sudo chown -R zonadart:zonadart /opt/zonadart/
-    sudo cp zonadart /opt/zonadart/
-		sudo cp borrar-snaps.sh /opt/zonadart/
-		sudo cp desfrag.pl /opt/zonadart/
-		sudo cp frag.pl /opt/zonadart/
-		sudo cp compt.sh /opt/zonadart/
-		sudo cp repo.sh /opt/zonadart/
-		sudo cp sec.sh /opt/zonadart/
-		sudo cp copsec.sh /opt/zonadart
-		sudo cp serv.sh /opt/zonadart/
-		sudo cp sist.sh /opt/zonadart/
-		sudo cp xarx.sh /opt/zonadart/
-		sudo cp util.sh /opt/zonadart/
-		sudo cp sortida.sh /opt/zonadart/
-		sudo chown zonadart:zonadart /opt/zonadart/*
-    sudo chmod 755 /opt/zonadart/*
-    sudo ln -s /opt/zonadart/zonadart /usr/local/bin/zonadart
-# fi
+    sudo mkdir /usr/local/zonadart
+    sudo chown -R $id /usr/local/zonadart
+    cp zonadart /usr/local/zonadart/
+    chmod +x /usr/local/zonadart/zonadart
+    sudo ln -s /usr/local/zonadart/zonadart /usr/bin/zonadart
+fi
 echo
-echo "Instal·lació completa. Es pot tancar aquest terminal."
+echo "Install complete. You can close this terminal."
 sleep 2
